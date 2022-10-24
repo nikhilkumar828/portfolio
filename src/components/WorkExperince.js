ecd83dd83287f1d9f4a188faf12be31e0e23b647
Nikhil Vallore <nvallore@salesforce.com>
2022-10-24 14:37:57 +0530

    ui cahnges

--- src/components/WorkExperince.js
+++ src/components/WorkExperince.js
@@ -10,11 +10,11 @@ export default function WorkExperience() {
         <h1 className="sm:text-4xl text-3xl font-medium title-font text-white mb-12">
           Work Experience
         </h1>
-        <div className="flex flex-col m-4">
+        <div className="flex flex-col m-4 border">
           {workExp.map((workExp) => (
               <div class="max-w-sm w-full max-w-full lg:flex text-left">
-              <img class="h-48 lg:h-auto w-48 ml-2 bg-white mb-2" src={workExp.image} alt="Avatar of Company" />
-              <div class="border-r border-b border-l border-gray-400 lg:border-l-0 lg:border-t lg:border-gray-400 text-white rounded-b lg:rounded-b-none lg:rounded-r p-4 flex flex-col justify-between leading-normal mb-4">
+              <img class="h-48 w-48 ml-2 bg-white self-center " src={workExp.image} alt="Avatar of Company" />
+              <div class="border-gray-400 lg:border-l-0 lg:border-t lg:border-gray-400 text-white rounded-b lg:rounded-b-none lg:rounded-r p-4 flex flex-col justify-between leading-normal mb-4">
                       <div class="mb-8">
                           <div class="font-bold text-xl mb-1">{workExp.company}</div>
                           <p class="text-sm text-white-10 flex items-center mb-3">
